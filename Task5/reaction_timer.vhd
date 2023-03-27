bb6d99437575f7a029f73172c2b29c76d8bedaca
Gabriele Bona <s281967@studenti.polito.it>
2023-03-27 18:42:15 +0200

    reset counter millisec era sbagliato

--- Task5/reaction_timer.vhd
+++ Task5/reaction_timer.vhd
@@ -16,7 +16,7 @@ SIGNAL ms, q_sr, delay, en_delay, en_reaction, resn_ms, is_bigger_ms, is_delay: 
 SIGNAL rn, sn: STD_LOGIC;
 SIGNAL Q_ms, Q_reaction: UNSIGNED(15 DOWNTO 0);
 SIGNAL Q_delay, th_delay: UNSIGNED (7 DOWNTO 0);
-CONSTANT millisecond: UNSIGNED:= "1100001101010000" -- 50'000 clock periods are 1ms
+CONSTANT millisecond: UNSIGNED:= "1100001101010000"; -- 50'000 clock periods are 1ms
 
 COMPONENT comparator IS 
 GENERIC (N: INTEGER:= 4);
@@ -41,21 +41,21 @@ output: OUT std_logic_vector(0 to 6));
 END COMPONENT;
 
 BEGIN
-resn_ms <= KEY(0) OR NOT is_bigger_ms;
+resn_ms <= KEY(0) AND NOT is_bigger_ms;
 en_delay<= ms AND NOT is_delay;
 th_delay <= UNSIGNED(SW);
 LEDR(0)<=is_delay;
 en_reaction <= is_delay AND q_sr AND ms;
 rn <= NOT KEY(3);
 sn <= NOT KEY(0);
-msc: synchronous_counter PORT MAP (enable=>'1', clock=>CLOCK_50, clear=>resn_ms, Q=>Q_ms);
+msc: synchronous_counter PORT MAP (enable=>'1', clock=>CLOCK_50, clear=>resn_ms, Q=>Q_ms); --millisecond counter
 cms: comparator GENERIC MAP(N => 16)
     PORT MAP (input=>Q_ms, threshold=>millisecond, is_bigger=>is_bigger_ms, is_equal=>ms);
 dlc: synchronous_counter GENERIC MAP (N=>8)
     PORT MAP (enable=>en_delay, clock=> CLOCK_50, clear=>KEY(0), Q=>Q_delay);
 cmd: comparator GENERIC MAP (N=>8)
     PORT MAP (input=>Q_delay, threshold=>th_delay, is_equal=> is_delay, is_bigger=>open);
-srl: SR_latch PORT MAP (Clk=>CLOCK_50, R=>rn, S=>sn, Q=>q_sr);
+ltc: SR_latch PORT MAP (Clk=>CLOCK_50, R=>rn, S=>sn, Q=>q_sr);
 crc: synchronous_counter PORT MAP (enable=>en_reaction, clock=>CLOCK_50, clear=>KEY(0), Q=>Q_reaction);
 D0: display_7seg PORT MAP (input => Q_reaction(3 downto 0), output => HEX0);
 D1: display_7seg PORT MAP (input => Q_reaction(7 downto 4), output => HEX1);
